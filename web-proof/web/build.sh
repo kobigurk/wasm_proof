f86eb284c3399599d6fa453ab084737f9d820d0f
Kobi Gurkan <kobigurk@gmail.com>
2019-02-02 13:59:40 +0200

    fix: adds missing out-dir

--- web-proof/web/build.sh
+++ web-proof/web/build.sh
@@ -1,2 +1,2 @@
 cargo +nightly build --target=wasm32-unknown-unknown --lib --release
-wasm-bindgen ../target/wasm32-unknown-unknown/release/web_proof.wasm --out-dir
+wasm-bindgen ../target/wasm32-unknown-unknown/release/web_proof.wasm --out-dir .
